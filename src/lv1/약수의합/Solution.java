d7d88eb183cffe4b0d9e6f445ed670301a078e28
jeonghyeon-ohk <ok6737@naver.com>
2025-07-08 20:13:16 +0900

    lv1.약수의합

---
+++ src/lv1/약수의합/Solution.java
@@ -0,0 +1,9 @@
+package lv1.약수의합;
+
+import java.util.stream.LongStream;
+
+public class Solution {
+    public int solution(int n) {
+        return (int) LongStream.rangeClosed(1, n/2).filter(num -> n%num==0).sum() + n;
+    }
+}
