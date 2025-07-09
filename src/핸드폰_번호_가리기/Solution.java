ed7573c76424a9a9f175d902e59e229c743c615d
jeonghyeon-ohk <ok6737@naver.com>
2025-07-09 20:23:38 +0900

    핸드폰_번호_가리기

---
+++ src/핸드폰_번호_가리기/Solution.java
@@ -0,0 +1,17 @@
+package 핸드폰_번호_가리기;
+
+class Solution {
+
+    public String solution(String phone_number) {
+        int index = phone_number.length() - 4;
+        return "*".repeat(index) + phone_number.substring(index);
+    }
+
+//    public String solution(String phone_number) {
+//        int point = phone_number.length() - 4;
+//        StringBuilder sb = new StringBuilder("*".repeat(point));
+//
+//
+//        return sb.append(phone_number.substring(point)).toString();
+//    }
+}
