4069379ffd627fc43243f83550bf7eb1cf79c1ed
jeonghyeon-ohk <ok6737@naver.com>
2025-07-09 21:44:07 +0900

    약수의_개수와_덧셈

---
+++ src/약수의_개수와_덧셈/Solution.java
@@ -0,0 +1,21 @@
+package 약수의_개수와_덧셈;
+
+class Solution {
+    public int solution(int left, int right) {
+
+        int answer = 0;
+
+        for(int i=left; i <= right; i++) {
+            int count = 0;
+            for(int j=1; j <= i/2; j++) {
+                if(i % j == 0) {
+                    count++;
+                }
+            }
+            count++; // 자기 자신도 약수에 포함
+            answer += (count % 2 == 0) ? i : -i;
+        }
+
+        return answer;
+    }
+}
