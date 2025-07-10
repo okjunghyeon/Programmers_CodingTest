17477998b5354a0ea338bc6485a67f5b32e218f8
jeonghyeon-ohk <ok6737@naver.com>
2025-07-10 16:14:39 +0900

    lv1.문자열_다루기_기본

--- src/lv1/문자열_다루기_기본/Solution.java
+++ src/lv1/문자열_다루기_기본/Solution.java
@@ -11,15 +11,15 @@ package lv1.문자열_다루기_기본;
 //        return s.length() == 4 || s.length() == 6;
 //    }
 //}
-//class Solution {
-//    public boolean solution(String s) {
-//
-//        try {
-//            int n = Integer.parseInt(s);
-//        } catch (NumberFormatException e) {
-//            return false;
-//        }
-//
-//        return s.length() == 4 || s.length() == 6;
-//    }
-//}
+class Solution {
+    public boolean solution(String s) {
+
+        try {
+            int n = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return s.length() == 4 || s.length() == 6;
+    }
+}
