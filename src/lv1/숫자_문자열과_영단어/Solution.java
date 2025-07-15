fcfe98e105367349203eed14d7f4eb0aecef14af
jeonghyeon-ohk <ok6737@naver.com>
2025-07-15 21:19:26 +0900

    lv1.숫자_문자열과_영단어

---
+++ src/lv1/숫자_문자열과_영단어/Solution.java
@@ -0,0 +1,14 @@
+package lv1.숫자_문자열과_영단어;
+
+class Solution {
+    public int solution(String s) {
+
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < numbers.length; i++) {
+            s = s.replaceAll(numbers[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
