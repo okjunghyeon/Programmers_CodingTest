e86a8a6a232dbfd9e2e820f13d19b2d8e0fca362
jeonghyeon-ohk <ok6737@naver.com>
2025-07-18 13:54:52 +0900

    lv1._2016년

---
+++ src/lv1/_2016년/Solution.java
@@ -0,0 +1,36 @@
+package lv1._2016년;
+
+
+//class Solution {
+//    public String solution(int a, int b) {
+//        // 2016년 1월 1일은 금요일입니다.
+//        int[] days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//        String[] months = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+//
+//        int count = 0;
+//        // a-1 달 까지 총 일수를 구한다
+//        for (int i = 0; i < a - 1; i++) {
+//            count += days[i];
+//        }
+//
+//        // 구한 수에 +b를 한다.
+//        count += b;
+//
+//        // 값을 7로 나눈 나머지를 구한다.
+//        count %= 7;
+//
+//        // 요일의 index에 해당하는 날짜를 반환한다
+//        return months[count];
+//    }
+//}
+
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        // 2016년 1월 1일은 금요일입니다.
+        LocalDate date = LocalDate.of(2016, a, b);
+
+        return date.getDayOfWeek().toString().substring(0,3);
+    }
+}
