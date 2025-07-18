1c558c860e1731f1404faf7a20506fbe34110b79
jeonghyeon-ohk <ok6737@naver.com>
2025-07-18 18:59:53 +0900

    lv1.소수_찾기

---
+++ src/lv1/소수_찾기/Solution.java
@@ -0,0 +1,18 @@
+package lv1.소수_찾기;
+
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        return (int) IntStream.rangeClosed(2, n)
+                .filter(this::isPrimeNumber)
+                .count();
+    }
+
+    boolean isPrimeNumber(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
